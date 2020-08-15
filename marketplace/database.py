4024c5f87697218c0eacf1ccb4f4cae422c317ec
Thilagavathy <thilagarajesh98@gmail.com>
2020-08-15 16:14:42 +0800

    status code implemented for success

--- marketplace/database.py
+++ marketplace/database.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 def connect_db():
     db_connection_string = "postgresql://postgres:#Thilaga1094@localhost:5432/online_shopping"
     return create_engine(db_connection_string)
-
+@app.route('/')
+def index():
+    return "welcome to online shopping", 200
 db = connect_db()
 print("Database connected successfully")
 if __name__ == "__main__":
