170c2cb688ac7bb3e08bb59436492786e9bf205a
Thilagavathy <thilagarajesh98@gmail.com>
2020-08-19 13:47:35 +0800

    changes in token_required

--- marketplace/api.py
+++ marketplace/api.py
@@ -1,4 +1,5 @@
-from flask import session, request, jsonify, Flask
+from flask import session,request,jsonify,Flask
+import os
 import jwt
 from jwt import PyJWT
 import datetime
@@ -6,7 +7,6 @@ from database import *
 from sqlalchemy.orm import sessionmaker
 from functools import wraps
 from models import *
-import os
 
 app = Flask(__name__)
 app.config['secret_key'] = os.urandom(50)
@@ -15,19 +15,13 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = request.args.get('token')
         if not token:
-            return jsonify({'message': 'Token is missing'}), 403
+            return jsonify({'message':'Token is missing'}), 403
         try:
-            details = jwt.decode(token, app.config['secret_key'])
+            details = jwt.decode(token,app.config['secret_key'])
         except:
-            return jsonify({'message': 'Token is invalid'}), 403
+            return jsonify({'message':'Token is invalid'}), 403
         return f(*args, **kwargs)
-
     return decorated
-
-@app.route('/')
-def index():
-    return "<h1>WELCOME TO MARKET PLACE</h1>",200
-
 
 @app.route('/user', methods=['POST'])
 def validate_user():
@@ -36,7 +30,7 @@ def validate_user():
     user_details = db.execute("SELECT * from users WHERE name= \'{}\'  and password= \'{}\'".format(username, password))
     data = user_details.fetchone()
     if data is not None:
-        token = jwt.encode({'name': username, 'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=30)}, app.config['secret_key'])
+        token = jwt.encode({'name': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['secret_key'])
         return jsonify({'token': token.decode('UTF-8'), 'message': 'logged in successfuly'}), 200
     else:
         return jsonify({'message': 'Sorry,cannot access: Unauthorized'}), 401
@@ -48,34 +42,33 @@ def display_categories():
     categories = session.query(Category).all()
     formatted_result = [display(row) for row in categories]
     return jsonify(formatted_result)
-
 def display(row):
     return "id: "+str(row.id)+","+"name: "+str(row.name)
-
 
 @app.route('/categories/<category_id>/', methods=['GET'])
 @token_required
 def get_items(category_id):
-    item_details = session.query(Item).join(Category).filter(Category.id == category_id).all()
-    for item in item_details:
-        return "Product ID: {} Product Name: {} ".format(item.id, item.name)
+    items = session.query(Category).join(Item).filter(Category.id == category_id).all()
+    formatted_result = [display(item) for item in items]
+    return jsonify(formatted_result)
+def display(item):
+    return "id: "+str(item.id)+","+"name: "+str(item.name)
 
-
-@app.route('/items/<item_id>/', methods=['GET'])
+@app.route('/items/<id>/', methods=['GET'])
 @token_required
-def get_items_details(item_id):
-    item_details = session.query(Item).join(Seller).filter(Item.id == item_id).all()
-    for row in item_details:
-            return "product ID:{} product Name:{}  description:{}".format(row.id, row.name, row.price, row.description)
-
+def get_items_details(id):
+    item_id = id
+    item_details = session.query(Item).filter_by(id=item_id).one()
+    formatted_result = [dict(row) for row in item_details]
+    return jsonify(formatted_result)
 
 @app.route('/cart', methods=['POST'])
 @token_required
 def add_item_to_cart():
-    item_id = request.form.get('item_id')
-    user_id = request.form.get('user_id')
-    quantity = request.form.get('quantity')
-    item_to_be_added = Cart(item_id=item_id, user_id=user_id, quantity=quantity)
+    item_identity = request.form.get('item_identity')
+    user_identity = request.form.get('user_identity')
+    required_quantity = request.form.get('required_quantity')
+    item_to_be_added = Cart(item_id=item_identity, user_identity=user_identity, required_quantity=required_quantity)
     session.add(item_to_be_added)
     session.commit()
     return "Data added successfully"
@@ -83,35 +76,29 @@ def add_item_to_cart():
 @app.route('/cart', methods=['PUT'])
 @token_required
 def update_quantity():
-    item_id = request.form.get('item_id')
-    user_id = request.form.get('user_id')
-    quantity = request.form.get('quantity')
-    update_quantity = session.query(Cart).filter_by(item_id=item_id, user_id=user_id).one()
-    update_quantity.quantity = quantity
+    item_identity = request.form.get('item_identity')
+    user_identity = request.form.get('user_identity')
+    desired_quantity = request.form.get('desired_quantity')
+    update_quantity = session.query(Cart).filter_by(item_identity=item_identity, user_identity=user_identity,
+                                                    desired_quantity=desired_quantity).one()
+    update_quantity.desired_quantity = desired_quantity
     session.add(update_quantity)
     session.commit()
     return "Data updated successfully"
 
-
 @app.route('/cart/<item_id>', methods=['DELETE'])
-@token_required
 def remove_item_from_cart(item_id):
-    item_id = item_id
-    user_id = request.form['user_id']
-    remove_item = session.query(Cart).filter_by(item_id=item_id, user_id=user_id).one()
+    item_identity = item_id
+    user_identity = request.form['user_identity']
+    remove_item = session.query(Cart).filter_by(item_id=item_identity, user_identity=user_identity).one()
     session.delete(remove_item)
-    session.commit()
     return "deleted successfully"
 
-
 @app.route('/cart/<user_id>', methods=['GET'])
-@token_required
 def view_cart_items(user_id):
-    cart_items = db.execute("select items.id,items.name,items.price,items.description from items inner join  "
-                            "carts on carts.item_id=items.id where user_id=\'{}\'".format(user_id))
-    cart_items = [dict(row) for row in cart_items]
-    return jsonify(cart_items)
-
+    for item, cart in session.query(Item, Cart).filter(Cart.user_id == user_id).all():
+        return "Product ID: {} product Name: {} product price: {} quantity: {}".format(item.item_id, item.name,
+                                                                                     item.price, cart.quantity)
 
 @app.route('/logout', methods=['DELETE'])
 def logout():
@@ -119,6 +106,10 @@ def logout():
         session.pop('name', None)
         session.pop('password', None)
     return "logged out successfully"
+
+@app.route('/')
+def index():
+    return "<h1>WELCOME TO MARKET PLACE</h1>"
 
 
 Session = sessionmaker(bind=db)
