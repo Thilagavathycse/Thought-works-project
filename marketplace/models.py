c771652a3a2e91ed44b6e09208cdcb30ed376300
Thilagavathy <thilagarajesh98@gmail.com>
2020-08-21 22:48:06 +0800

    models.py changed

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,27 +1,44 @@
-from flask import session, Blueprint
-from models import *
-from app import *
-# coding=UTF-8
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, ForeignKey, Float, Text
 
 
-categories_pb = Blueprint('categories_pb', __name__)
+Base = declarative_base()
 
-@categories_pb.route('/categories', methods=['GET'])
-@token_required
-def display_categories():
-    categories = session.query(Category).all()
-    formatted_result = [display(row) for row in categories]
-    return jsonify(formatted_result)
-def display(row):
-    return "id: "+str(row.id)+","+"name: "+str(row.name)
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text(20), nullable=False)
+    password = Column(Text(20), nullable=False)
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text(20), nullable=False)
 
 
-@categories_pb.route('/categories/<category_id>/', methods=['GET'])
-@token_required
-def get_items(category_id):
-    items = session.query(Category).join(Item).filter(Category.id == category_id).all()
-    formatted_result = [display(item) for item in items]
-    return jsonify(formatted_result)
-def display(item):
-    return "id: "+str(item.id)+","+"name: "+str(item.name)
+class Seller(Base):
+    __tablename__ = 'sellers'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text(20), nullable=False)
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Text(5), primary_key=True)
+    name = Column(Text(20), nullable=False)
+    seller_id = Column(Integer, ForeignKey('sellers.id'))
+    price = Column(Float, nullable=False)
+    description = Column(Text(100), nullable=True)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+
+class Cart(Base):
+    __tablename__ = 'carts'
+    cart_id = Column(Integer, primary_key=True, autoincrement=True)
+    item_id = Column(Text(5), ForeignKey('items.id'), nullable=False)
+    user_id = Column(Text(5), ForeignKey('users.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+
+
+
+
 
