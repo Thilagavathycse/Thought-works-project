caf6d126c3400abf194670613172bdbf1f097664
Thilagavathy <thilagarajesh98@gmail.com>
2020-08-30 20:50:07 +0800

    added templates files

--- templates/item.js
+++
@@ -1,9 +0,0 @@
-function categoryitem() {
-    const url = 'http://127.0.0.1:5000/categories/1'
-    fetch(url)
-    .then(response => response.json())  
-    .then(json => {
-        console.log(json);
-        document.getElementById("demo").innerHTML = JSON.stringify(json)
-    })
-}
